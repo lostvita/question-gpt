8b7f52cefb6ce5cc7201a32e6eb5bb6143a76559
lostvita <lostmyvita@gmail.com>
2023-09-16 00:15:47 +0800

    feat: init pro

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div className='container max-w-none h-screen flex flex-col justify-center items-center text-lg'>
+      home page
+    </div>
+  )
+}
+
+export default Home
